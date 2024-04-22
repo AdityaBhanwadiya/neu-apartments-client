1387a33c740622ffa4ed0171c76b061dbfc824d8
aditya222001 <adityapatel020201@gmail.com>
2024-04-22 13:07:31 +0530

    client commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About NEU Apartments</h1>
+      <p className='mb-4 text-slate-700'>NEU Apartments is a student community, where students can list the apartments the want to lease or sell and fellow Northeastern students can view these listings.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help new enrolled students to have a standard portal where they can find affordable apartments in safe environments around Boston Campus.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}

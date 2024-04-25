4f54c1ae45339dc8c22bcb67818eba4b57aace4f
aditya222001 <adityapatel020201@gmail.com>
2024-04-26 01:45:54 +0530

    vite update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://neu-apartments-server.onrender.com',
+        target: 'https://neu-apartments-client.onrender.com',
         secure: false,
       },
     },
